74ae1af8c0833629623afb42a780665bd30d1eb4
allen.hutchison <allen.hutchison@27ccdd19-9b1f-0410-aef2-3b794fcb9892>
2008-11-25 21:27:58 +0000

    Updating the update.pl script to handle problems on the lines

--- trunk/update.pl
+++ trunk/update.pl
@@ -12,39 +12,27 @@ my $xp = XML::XPath->new(xml => $in);
 my %info;
 my $lineSet = $xp->find('//*[@id="service-board"]/dl[@id="lines"]/dt');
 my $statusSet = $xp->find('//*[@id="service-board"]/dl[@id="lines"]/dd');
-while ($lineSet->get_nodelist) {
-    my $line = $lineSet->pop()->findvalue('.//@class');
-    $line =~ s/\s+/ /g;
-    $key = $line;
-    $key =~ s/\s//g;
-    $key = lc($key);
+foreach my $node ($lineSet->get_nodelist) {
+  my $line = $node->findvalue('.//@class');
+  $line =~ s/\s+//g;
+  $line = lc($line);
+  $info{$line}{'name'} = $line;
 
-    my $status = $statusSet->pop()->findvalue('.//text()');
-    $status =~ s/\s+/ /g;
-    $info{$key}{'status'} = $status;
-    $info{$key}{'name'} = $line
-}
-
-my $nodeset = $xp->find('//*[@class="col2"]/div');
-foreach my $node ($nodeset->get_nodelist) {
-    my $detail = $node->findvalue('./p[1]/text()');
-    $detail =~ s/\s+/ /g;
-    $detail =~s/^(.*):(.*)/$2/;
-    $line = $1;
-    $detail =~ s/^ //;
-    $detail =~ s/ $//;
-    $line =~ s/\s//g;
-    $line = lc($line);
-    $line =~ s/line//;
-    if ($info{$line}) {
-    	$info{$line}{'message'} = $detail;
-   } 
+  my $status = $node->findvalue('.//following-sibling::dd[position()=1]/text()');
+  if ($status =~ /^\s+$/) {
+    $status = $node->findvalue('.//following-sibling::dd[@class="problem"]/h3/text()');
+    my $message = $node->findvalue('.//following-sibling::dd[@class="problem"]/div[@class="message"]/p/text()');
+    $message =~ s/^(.*): (.*)/$2/;
+    $message =~ s/\s+/ /g;
+    $info{$line}{'message'} = $message;
+  }
+  $info{$line}{'status'} = $status
 }
 
 my $date = `env TZ=Europe/London date --iso-8601=minutes`;
 chomp($date);
 print "<?xml version=\"1.0\"?>\n<tube date=\"$date\">\n";
 foreach $line (keys %info) {
-    print " <line name=\"$info{$line}{'name'}\" status=\"$info{$line}{'status'}\"".($time{$line} ? " time=\"$time{$line}\"" : "").">$info{$line}{'message'}<\/line>\n";
+    print " <line name=\"$info{$line}{'name'}\" status=\"$info{$line}{'status'}\">$info{$line}{'message'}<\/line>\n";
 }
 print "</tube>\n";
